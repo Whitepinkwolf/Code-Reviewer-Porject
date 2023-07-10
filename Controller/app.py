d74b2c436a162e92c703f7c4b814ff2acd0d9460
Lililiiz <565790159@qq.com>
2023-07-10 12:01:06 +0800

    commentWidget新增

---
+++ Controller/app.py
@@ -0,0 +1,70 @@
+import os
+import sys
+
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from UI.Main import Ui_MainWindow
+from UI.commentWidget import Ui_Form
+
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.setupUi(self)
+        self.connectSignalsSlots()
+        self.set_file_tree()
+
+        self.commentWidget = None  # 添加该行
+
+    def connectSignalsSlots(self):
+        self.treeView.doubleClicked.connect(self.file_tree_clicked)
+
+    def file_tree_clicked(self):
+
+        # 策略双击槽函数
+        index = self.treeView.currentIndex()
+        model = index.model()  # 请注意这里可以获得model的对象
+
+        item_path = model.filePath(index)
+
+        self.add_CommentWidget(item_path)
+
+        if not os.path.isdir(item_path):
+            try:
+                with open(item_path, 'r', encoding='utf-8') as file:
+                    file_content = file.read()
+                    print(file_content)  # 在这里可以使用文件内容进行进一步的处理
+                    self.commentWidget.plainTextEdit.setPlainText(file_content)
+            except FileNotFoundError:
+                print("文件不存在")
+            except IOError:
+                print("无法读取文件")
+
+    def add_CommentWidget(self, item_path):
+        commentWidget = comment_Widget()
+        self.commentTabWidget.addTab(commentWidget, item_path)
+
+        self.commentWidget = commentWidget  # 添加该行
+
+    def set_file_tree(self):
+        model = QtWidgets.QFileSystemModel()
+        model.setRootPath(QtCore.QDir.rootPath())
+        model.setReadOnly(False)
+        self.treeView.setModel(model)
+        self.treeView.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.treeView.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
+        self.treeView.setHeaderHidden(True)
+        self.treeView.hideColumn(1)
+        self.treeView.hideColumn(2)
+        self.treeView.hideColumn(3)
+
+class comment_Widget(QtWidgets.QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super(comment_Widget, self).__init__(parent)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win =  Window()
+    win.show()
+    sys.exit(app.exec())
---
+++ UI/commentWidget.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'commentWidget.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(927, 761)
+        self.plainTextEdit = CodeEditor(Form)
+        self.plainTextEdit.setGeometry(QtCore.QRect(0, 0, 1001, 891))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+
+from UI.CodeEditor import CodeEditor
