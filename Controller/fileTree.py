dd4a4c7348eb2b1d3fa6a64b39fc7b96d9afbb98
neuljh <13550190917@163.com>
2023-07-11 22:34:03 +0800

    lq和ljh同步

--- Controller/fileTree.py
+++ Controller/fileTree.py
@@ -9,6 +9,8 @@ import os
 from PyQt5.QtWidgets import  QTreeWidgetItem
 from PyQt5.QtCore import QModelIndex
 from PyQt5 import QtWidgets, QtCore
+from clang.cindex import File
+
 
 class FileTree:
     def __init__(self):
